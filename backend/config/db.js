7b052957f7dc4e8b032af3542b395e8c9579f4bd
Sanyukta Vishnu Mane <smane1@academian.com>
2025-07-02 10:57:50 +0530

    committt

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,15 +11,22 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Needed for AWS RDS without root cert
+const sequelize = new Sequelize(
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // Username
+  process.env.DB_PASS, // Password
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // For AWS RDS without root certificate
+      },
     },
-  },
-  logging: false,
-});
+    logging: false, // Disable SQL logging
+  }
+);
 
 module.exports = sequelize;
